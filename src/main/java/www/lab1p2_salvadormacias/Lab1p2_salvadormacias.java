b9b279430010bd65868b0fb4e178e45861f98139
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-01-19 13:37:14 -0600

    primmer commit

---
+++ src/main/java/www/lab1p2_salvadormacias/Lab1p2_salvadormacias.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package www.lab1p2_salvadormacias;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Apple
+ */
+public class Lab1p2_salvadormacias {
+
+    public static Scanner lea = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opcion = 0;
+        do {
+            System.out.println("---sistema de registro de usuarios----");
+            System.out.println("opciones\n1.registrar usuario\n2.listar todo\n3.listar por dominio");
+
+        } while (opcion != 4);
+
+    }//fin main
+}//fin clase
